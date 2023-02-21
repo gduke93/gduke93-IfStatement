bd84de018797ba3e62447f558c624c13db14e66f
gduke93 <grayduke93@gmail.com>
2023-02-21 20:51:07 +0000

    Completed Lab If Statement

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,13 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return 0;
+        int xx = 0;
+        if (bool == true){
+            xx = x;
+        } 
+        if (bool == false){
+            xx = y;
+        }
+        return xx;
     }
 }
